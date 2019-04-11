39e1d493169737b71ef4a0acc306c7255548e70a
Megan Fago <meganlaynefago@gmail.com>
2019-04-11 14:04:32 -0400

    Added methods in controllers

--- app/controllers/nail_polishes_controller.rb
+++ app/controllers/nail_polishes_controller.rb
@@ -1,2 +1,38 @@
 class NailPolishesController < ApplicationController
+
+    def index
+        @nailpolishes = NailPolish.all 
+        render json: @nailpolishes, status: ok
+    end 
+
+    def show 
+        @nailpolish = NailPolish.find(params[:id])
+        render json: @nailpolishes, status: ok
+    end 
+
+    def create 
+        @nailpolish = NailPolish.create(nailpolish_params)
+        if @nailpolish
+            render json: @nailpolish, status: :ok
+        else
+            render json: {errors: @nailpolish.errors.full_messages},
+            status: :unprocessable_entity
+        end 
+    end 
+
+    def update 
+        @nailpolish = NailPolish.find(params[:id])
+        @nailpolish.update(nailpolish_params)
+    end 
+
+    def destroy
+        @nailpolish = NailPolish.find(params[:id])
+        @nailpolish.destroy
+    end 
+
+    private 
+
+    def nailpolish_params
+        params.permit(:id, :name, :color, :brand_name, :shade, :rating, :review)
+    end 
 end
